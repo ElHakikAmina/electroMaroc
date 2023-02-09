a9a01391415b5bd5107389ad4345d47f8d2add18
ElHakikAmina <amina.el.hakik2@gmail.com>
2023-02-09 10:21:08 +0100

    e-commerce

---
+++ views/pages/category.php
@@ -0,0 +1,11 @@
+
+<div class="d-flex flex-wrap my-5">
+    <?php foreach($products as $p):?>
+    <div class="mb-5 mx-4">
+            <a href="http://localhost/e-commerce/product/<?php echo $p['id']; ?>" class="text-decoration-none">
+            <img src="http://localhost/e-commerce/views/pages/affiche_img_produit.php?id=<?php echo $p['id'];?>" style="width:180px; height:220px;" alt=""><br>
+            <span class="ChakraPetch text-muted"><?php  echo $p['libelle'];?></span><br>
+            <span class="ChakraPetch text-success"><?php  echo $p['prix_final'];?>MAD</span></a>  <br>
+    </div>
+<?php endforeach; ?> 
+</div>
